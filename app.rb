525933ffccb0f7e025567cb81426976c5c2c1ae8
bkrigel <benjamin.krigel@gmail.com>
2018-06-29 10:54:23 -0400

    Easy enough

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ben"
+  end
+
+  get '/hometown' do
+    "My hometown is San Jose"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'This Is The Place' by the Red Hot Chili Peppers"
+  end
+
 end
